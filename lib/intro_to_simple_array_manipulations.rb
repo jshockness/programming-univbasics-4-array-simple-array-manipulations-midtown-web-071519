46220c5f280e178ef64c1229fc08aa981d71754f
Jason Shockness <jshock12@gmail.com>
2019-07-12 09:01:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,13 @@
 def using_push(array, string)
   array.push(strings)
 end
+
+def using_unshift(array, strings)
+  array.unshift(strings)
+end
+
+def using_pop(array)
+  array.pop 
+end
+
+def pop_with_args
